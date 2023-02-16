2e09c219dfe4ea10431b4e1f4fa288f6e66a4966
smhrd <smhrd@220.80.88.113>
2023-02-16 16:48:38 +0900

    test (평광)

--- ProjectGitTest/src/main/java/com/test/Test.java
+++ ProjectGitTest/src/main/java/com/test/Test.java
@@ -5,7 +5,8 @@ public class Test {
 	public static void main(String[] args) {
 		System.out.println("첫번째 푸시!");
 		System.out.println("두번째 푸시!");
-		System.out.println("충돌내보자");		
+		System.out.println("충돌내보자");	
+		System.out.println("평광");
 	}
 
 }
