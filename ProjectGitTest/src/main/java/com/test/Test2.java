8660efa2923ab5e7590c897a99b8efdab55c725a
smhrd <smhrd@218.149.140.8>
2023-02-16 16:51:59 +0900

    수정하고 다시 올리기

--- ProjectGitTest/src/main/java/com/test/Test2.java
+++ ProjectGitTest/src/main/java/com/test/Test2.java
@@ -3,8 +3,8 @@ package com.test;
 public class Test2 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		 System.out.println("새로운 파일 추가!!!");
+		 System.out.println("새로운 파일 추가!!!");
 	}
 
 }
